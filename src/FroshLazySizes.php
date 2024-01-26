17763c0b3941eea5b790ca5a49ca133d50773d5d
Stefan Poensgen <mail@stefanpoensgen.de>
2024-01-26 23:02:07 +0100

    feat: shopware 6.6 compatibility (#22)
    ---------
    
    Co-authored-by: Shyim <github@shyim.de>
    Co-authored-by: tinect <s.koenig@tinect.de>

--- src/FroshLazySizes.php
+++ src/FroshLazySizes.php
@@ -1,9 +1,9 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Frosh\LazySizes;
 
 use Shopware\Core\Framework\Plugin;
 
-class FroshLazySizes extends Plugin
-{
-}
+class FroshLazySizes extends Plugin {}
