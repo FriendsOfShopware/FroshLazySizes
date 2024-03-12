79154d7bd43ac4e32391a67202317f7fb485b8f8
tinect <s.koenig@tinect.de>
2024-03-12 21:48:00 +0100

    feat: remove object-fit polyfill of lazySizes

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -2,7 +2,6 @@ import 'lazysizes';
 import 'lazysizes/plugins/native-loading/ls.native-loading';
 import 'lazysizes/plugins/attrchange/ls.attrchange';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import 'lazysizes/plugins/object-fit/ls.object-fit';
 
 document.addEventListener('lazybeforesizes', (e) => {
     const aspectRatio = e.target.getAttribute('data-aspectratio');
