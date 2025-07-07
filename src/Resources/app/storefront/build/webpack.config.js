43b3cf224337baba5bc70c38ca0c39cdde25dfaa
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2025-07-07 15:05:42 +0200

    chore: code style format

--- src/Resources/app/storefront/build/webpack.config.js
+++ src/Resources/app/storefront/build/webpack.config.js
@@ -3,8 +3,8 @@ module.exports = function (params) {
     return {
         resolve: {
             modules: [
-                `${params.basePath}/Resources/app/storefront/node_modules`
-            ]
-        }
+                `${params.basePath}/Resources/app/storefront/node_modules`,
+            ],
+        },
     };
 };
